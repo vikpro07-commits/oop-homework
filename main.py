3df573e6f5361effac0932e8e1a67002af820e7e
vikpro07-commits <vikpro07@gmail.com>
2026-03-14 20:06:24 +0300

    Add homework solution

---
+++ main.py
@@ -0,0 +1,62 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and \
+           course in self.courses_attached and \
+           course in student.courses_in_progress:
+
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+
+        else:
+            return 'Ошибка'
+
+
+# Проверка работы
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+reviewer = Reviewer('Some', 'Buddy')
+reviewer.courses_attached += ['Python']
+
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
+
+
+# Проверка наследования
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))   # True
+print(isinstance(reviewer, Mentor))   # True
+
+print(lecturer.courses_attached)      # []
+print(reviewer.courses_attached)      # []
